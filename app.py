464387e7bd7ac2fc5cf2ad20e121d659587dcda3
theprakharsaxena <prakharsaxena5125@gmail.com>
2026-09-21 21:45:55 +0530

    refactor: remove unused seaborn and tensorflow imports

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import joblib
 import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-import seaborn as sns
-from tensorflow.keras.models import load_model
 import time
 
 # Page Configuration
